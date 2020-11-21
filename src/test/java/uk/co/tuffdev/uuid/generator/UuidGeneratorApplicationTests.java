6a88509892b2980d3cdf34a0ae3fe5674bb60573
Tuff-Dev <tuff2dev@gmail.com>
2020-11-21 17:16:21 +0000

    initial add

---
+++ src/test/java/uk/co/tuffdev/uuid/generator/UuidGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.tuffdev.uuid.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UuidGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
