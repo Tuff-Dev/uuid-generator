490e307be6fa5a1efa51eb851643b569ac23ac81
Tuff-Dev <tuff2dev@gmail.com>
2020-11-21 17:25:06 +0000

    Add controller

---
+++ src/main/java/uk/co/tuffdev/uuid/generator/controller/MainController.java
@@ -0,0 +1,17 @@
+package uk.co.tuffdev.uuid.generator.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/generate")
+    public ResponseEntity<UUID> generateUUID() {
+        return ResponseEntity.ok(UUID.randomUUID());
+    }
+
+}
